e72d32f71af714d77cc0558ab60b753c4e648882
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2014-05-23 12:35:29 +0200

    fehlende variable in TYPO3 6.2.3

--- ext_tables.php
+++ ext_tables.php
@@ -1,14 +1,15 @@
 <?php
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
+$_EXTKEY = 'mkphpids';
 $TCA['tx_mkphpids_log'] = array (
 	'ctrl' => array (
-		'title'     => 'LLL:EXT:mkphpids/locallang_db.xml:tx_mkphpids_log',		
-		'label'     => 'uid',	
+		'title'     => 'LLL:EXT:mkphpids/locallang_db.xml:tx_mkphpids_log',
+		'label'     => 'uid',
 		'tstamp'    => 'tstamp',
 		'crdate'    => 'crdate',
 		'cruser_id' => 'cruser_id',
-		'default_sortby' => 'ORDER BY crdate',	
-		'delete' => 'deleted',	
+		'default_sortby' => 'ORDER BY crdate',
+		'delete' => 'deleted',
 		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY).'tca.php',
 		'iconfile'          => t3lib_extMgm::extRelPath($_EXTKEY).'icon_tx_mkphpids_log.gif',
 	),
@@ -16,12 +17,12 @@ $TCA['tx_mkphpids_log'] = array (
 
 $TCA['tx_mkphpids_cache'] = array (
 	'ctrl' => array (
-		'title'     => 'LLL:EXT:mkphpids/locallang_db.xml:tx_mkphpids_cache',		
-		'label'     => 'uid',	
+		'title'     => 'LLL:EXT:mkphpids/locallang_db.xml:tx_mkphpids_cache',
+		'label'     => 'uid',
 		'tstamp'    => 'tstamp',
 		'crdate'    => 'crdate',
 		'cruser_id' => 'cruser_id',
-		'default_sortby' => 'ORDER BY crdate',	
+		'default_sortby' => 'ORDER BY crdate',
 		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY).'tca.php',
 		'iconfile'          => t3lib_extMgm::extRelPath($_EXTKEY).'icon_tx_mkphpids_cache.gif',
 	),
@@ -30,7 +31,7 @@ $TCA['tx_mkphpids_cache'] = array (
 
 if (TYPO3_MODE == 'BE') {
 	t3lib_extMgm::addModulePath('tools_txmkphpidsM1', t3lib_extMgm::extPath($_EXTKEY) . 'mod1/');
-		
+
 	t3lib_extMgm::addModule('tools', 'txmkphpidsM1', '', t3lib_extMgm::extPath($_EXTKEY) . 'mod1/');
 }
 t3lib_extMgm::addStaticFile($_EXTKEY, './', 'MK PHPIDS');
