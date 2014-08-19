16bf870f6769dc59d79f9b7a1dade199ab7d0cd4
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2014-08-19 14:05:19 +0200

    kompatibilität angepasst

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.1.6-0.0.0',
-			'typo3' => '4.4.0-0.0.0',
+			'typo3' => '4.4.0-6.2.99',
 			'cms' => '',
 			'rn_base' => '',
 		),
